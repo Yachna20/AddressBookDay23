e9eb08347bc7f756397bb9bd3ebdea79f9837c48
Yachna20 <thakreyachna20@gmail.com>
2022-12-27 17:43:45 +0530

    [Yachna]-Create basic contact book

---
+++ AddressBookDay23/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook add = new AddressBook("Yachna", "Thakre", "GajananColony", "Gondia", "Maharashtra", "thakreyachna20@gmail.com", 4400011, 8989036339);
+            add.DisplayContacts();
+        }
+    }
+}
+
